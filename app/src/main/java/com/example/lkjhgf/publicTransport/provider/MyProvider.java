f40e87578827a852f2804b949f0e28ec369a1842
Murphius <merle1996@gmx.net>
2020-05-08 15:48:43 +0200

    Verallgemeinerung für beliebige Verkehrsverbunde
    Umsetzung für den VRR
    Entsprechende anpassung des Codes

---
+++ app/src/main/java/com/example/lkjhgf/publicTransport/provider/MyProvider.java
@@ -0,0 +1,4 @@
+package com.example.lkjhgf.publicTransport.provider;
+
+public class MyProvider {
+}
---
+++ app/src/main/java/com/example/lkjhgf/publicTransport/provider/MyVRRprovider.java
@@ -0,0 +1,4 @@
+package com.example.lkjhgf.publicTransport.provider;
+
+public class MyVRRprovider extends MyProvider {
+}
